d2698aac8b11465209ec23147be220e1d65b6964
Sai Krishna Chaitanya Annavazala <106294457+SaiKrishnaChaitanya24@users.noreply.github.com>
2025-07-25 15:53:22 -0400

    Update Fernet.sh

--- Scripts/Fernet.sh
+++ Scripts/Fernet.sh
@@ -76,7 +76,7 @@ log_run /usr/local/lib/python3.12/dist-packages/diciphr/scripts/n4_bias_correcti
 
 log_run /usr/local/lib/python3.12/dist-packages/diciphr/scripts/extract_gaussian_shells.py -i $tmpdir/dwi_N4.nii.gz -o $tmpdir/dwi_N4_gaussian.nii.gz 
 
-if [ $? -eq 2 ]; then
+if [[ "$shell_output" == *"NO_GAUSSIAN_SHELLS_DETECTED"* ]]; then
     log_info "Skipping Fernet: No Gaussian shells detected."
 else
     fernet_base=$outdir/${subject}
